bd74c287b6a6fd1dd3e3a3f9f954b3e680cbdf6d
nab <nabilibarissen2003@gmail.com>
2023-12-02 13:53:13 -0500

    init

---
+++ build/CMakeFiles/voxel.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/voxel.dir/glad.c.o"
+  "CMakeFiles/voxel.dir/glad.c.o.d"
+  "CMakeFiles/voxel.dir/src/main.cpp.o"
+  "CMakeFiles/voxel.dir/src/main.cpp.o.d"
+  "voxel"
+  "voxel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/voxel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
